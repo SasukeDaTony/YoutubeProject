4a9d37cb9af12c06959ae4a1e55960c333ba37a6
SasukeDaTony <tonytaylor@pursuit.org>
2022-11-19 16:17:39 -0500

    Day 1 some Functionality

---
+++ src/components/SearchBarForm.js
@@ -0,0 +1,10 @@
+export default function SearchBarForm() {
+  return (
+    <div>
+      <form>
+        <input type="text" value="Search"></input>
+        <input type="submit"></input>
+      </form>
+    </div>
+  );
+}
